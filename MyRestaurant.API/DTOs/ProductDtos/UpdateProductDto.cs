91a8c10d2895060b7e322f0395eafb23e3a38310
erhangndz <erhangndz@live.com>
2025-09-18 21:58:33 +0300

    Add project files.

---
+++ MyRestaurant.API/DTOs/ProductDtos/UpdateProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRestaurant.API.DTOs.ProductDtos
+{
+    public class UpdateProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
